432dcfc2193362144870d6f533ff80da55f70ec0
sheep2moon <kon.iwan17@gmail.com>
2021-08-02 12:36:40 +0200

    adding customize instruction

--- src/pages/Order.js
+++ src/pages/Order.js
@@ -3,6 +3,7 @@ import styled from "styled-components";
 import RangeInput from "../components/Customize/RangeInput";
 import Button from "../components/Button";
 import SelectColor from "../components/Customize/SelectColor";
+import infoImg from "../assets/dimensions.svg";
 
 const Customize = () => {
   const [price, setPrice] = useState(200);
@@ -12,54 +13,59 @@ const Customize = () => {
   const [color, setColor] = useState("#000");
 
   useEffect(() => {
-    const p = (length * width * thickness) / 300;
+    const p = ((length * width) / 100) * 3 + thickness * 20;
     setPrice(parseInt(p));
   }, [length, width, thickness]);
 
   return (
     <CustomizeContainer>
       <h1>Stwórz własne biurko</h1>
-      <FormContainer>
-        <InputInfo>
-          <label htmlFor="length">Dłguość</label>
-          <span>{length}cm</span>
-        </InputInfo>
-        <RangeInput
-          onChange={(e) => setLength(e.target.value)}
-          name="length"
-          min="80"
-          max="240"
-          step="10"
-        />
-        <InputInfo>
-          <label htmlFor="width">Szerokość</label>
-          <span>{width}cm</span>
-        </InputInfo>
-        <RangeInput
-          onChange={(e) => setWidth(e.target.value)}
-          name="width"
-          min="50"
-          max="160"
-          step="5"
-        />
-        <InputInfo>
-          <label htmlFor="thickness">Grubość</label>
-          <span>{thickness}cm</span>
-        </InputInfo>
-        <RangeInput
-          onChange={(e) => setThickness(e.target.value)}
-          name="thickness"
-          min="2"
-          max="10"
-          step="1"
-        />
-        <SelectColor setColor={setColor} color={color} />
+      <Content>
+        <InfoContainer>
+          <img src={infoImg} alt="" />
+        </InfoContainer>
+        <FormContainer>
+          <InputInfo>
+            <label htmlFor="length">Długość</label>
+            <span>{length}cm</span>
+          </InputInfo>
+          <RangeInput
+            onChange={(e) => setLength(e.target.value)}
+            name="length"
+            min="80"
+            max="240"
+            step="10"
+          />
+          <InputInfo>
+            <label htmlFor="width">Szerokość</label>
+            <span>{width}cm</span>
+          </InputInfo>
+          <RangeInput
+            onChange={(e) => setWidth(e.target.value)}
+            name="width"
+            min="50"
+            max="160"
+            step="5"
+          />
+          <InputInfo>
+            <label htmlFor="thickness">Grubość</label>
+            <span>{thickness}cm</span>
+          </InputInfo>
+          <RangeInput
+            onChange={(e) => setThickness(e.target.value)}
+            name="thickness"
+            min="2"
+            max="10"
+            step="1"
+          />
+          <SelectColor setColor={setColor} color={color} />
 
-        <Summary>
-          <p>Cena: {price.toFixed(2)} zł</p>
-          <Button type="submit">Zamów</Button>
-        </Summary>
-      </FormContainer>
+          <Summary>
+            <p>Cena: {price.toFixed(2)} zł</p>
+            <Button type="submit">Zamów</Button>
+          </Summary>
+        </FormContainer>
+      </Content>
     </CustomizeContainer>
   );
 };
@@ -73,13 +79,30 @@ const CustomizeContainer = styled.main`
   align-items: center;
   color: ${({ theme }) => theme.secondary};
 `;
-
+const Content = styled.div`
+  width: 100%;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  @media screen and (max-width: 1000px) {
+    flex-direction: column;
+    row-gap: 2rem;
+  }
+`;
+const InfoContainer = styled.div`
+  width: 50%;
+  display: flex;
+  justify-content: center;
+  > img {
+    max-width: 500px;
+  }
+`;
 const FormContainer = styled.form`
   display: flex;
   flex-direction: column;
   align-items: center;
-  width: 100%;
-  max-width: 400px;
+  width: 50%;
+  max-width: 500px;
   border: 1px solid #ffffff20;
   padding: 1rem;
   background-color: ${({ theme }) => theme.primary};
